bf90e8914a604707138103eb8ad1af6b7d63d51d
ttonightt <ttonightt@outlook.com>
2025-08-31 17:38:51 +0200

    Added curve fit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
-import { onMounted, ref, useTemplateRef, watch } from "vue";
-const pi2 = Math.PI * 2;
+import fitCurve from "fit-curve";
+import { onMounted, onUpdated, ref, useTemplateRef, watch } from "vue";
+const pi = Math.PI;
+const pi2 = pi * 2;
 
 
 const vec = (x, y, z) => ({ x, y, z });
@@ -53,13 +55,9 @@ const intrinsicM = (f, plainW, plainH) => [
 	0, 0, 1
 ];
 
-const proj = (x0, y0, z0) => {
+const proj = (x0, y0, z0, target, camera, f) => {
 
-	const trg = vec(0, 0, 0);
-	const cam = vec(0, 100, 100);
-	const f = 100;
-
-	const E = lookAtM(trg, cam);
+	const E = lookAtM(target, camera);
 	const I = intrinsicM(f, window.innerWidth, window.innerHeight);
 
 	const V = [
@@ -67,6 +65,9 @@ const proj = (x0, y0, z0) => {
 		(E[4] * x0) + (E[5] * y0) +  (E[6] * z0) + E[7],
 		(E[8] * x0) + (E[9] * y0) + (E[10] * z0) + E[11]
 	];
+
+	//if (V[2] < 0)
+	//	return null;
 
 	const P = [
 		(I[0] * V[0]) + (I[1] * V[1]) + (I[2] * V[2]),
@@ -105,76 +106,123 @@ class Spine {
 }
 
 
+let cnv, ctx;
+
 const canvasRef = useTemplateRef("canvas-ref");
+
+const spine = new Spine(100, 20, 13);
+
+const render = () => {
+
+	const target = vec(0, 0, 0);
+	const camera = vec(-90, -90, 130);
+	const f = 100;
+
+	ctx.clearRect(0, 0, cnv.width, cnv.height);
+
+	const dt = 0.1 / spine.n;
+
+	const points = [];
+
+	for (let t = 0; t < pi2; t += dt) {
+
+		points.push( proj( spine.fx(t), spine.fy(t), spine.fz(t), target, camera, f ) );
+	}
+
+	const plen = points.length;
+
+	ctx.strokeStyle = "#ffffff";
+	ctx.lineCap = "round";
+
+	for (let i = 0; i < plen; i++) {
+
+		const perc = -2 * Math.abs( (i / plen) - 0.5 ) + 1;
+		ctx.lineWidth = 4 + (perc * 12);
+		//ctx.lineWidth = 1;
+
+		ctx.beginPath();
+
+		if (i === 0) {
+			ctx.moveTo(points[plen - 1][0], points[plen - 1][1]);
+		} else {
+			ctx.moveTo(points[i - 1][0], points[i - 1][1]);
+		}
+		ctx.lineTo(points[i][0], points[i][1]);
+		ctx.stroke();
+	}
+
+	const curves = fitCurve(points, 2);
+
+	ctx.lineWidth = 1;
+	ctx.strokeStyle = "red";
+	ctx.beginPath();
+	ctx.moveTo(curves[0][0], curves[0][1]);
+
+	for (let i = 0; i < curves.length; i++) {
+
+		ctx.bezierCurveTo(...curves[i][1], ...curves[i][2], ...curves[i][3]);
+	}
+
+	ctx.stroke();
+
+	ctx.beginPath();
+
+	ctx.fillStyle = "red";
+
+	for (let i = 0; i < curves.length; i++) {
+
+		ctx.moveTo(...curves[i][0]);
+		ctx.arc(...curves[i][0], 2, 0, pi2);
+	}
+
+	ctx.fill();
+
+
+	const Ox = window.innerWidth / 2, Oy = window.innerHeight / 2;
+
+	console.log(
+		`<?xml version="1.0" standalone="no"?>\n<svg width="5cm" height="4cm" version="1.1" xmlns="http://www.w3.org/2000/svg">\n\t<path d="M ${
+			(curves[0][0][0] - Ox).toFixed(4)
+		} ${
+			(curves[0][0][1] - Oy).toFixed(4)
+		}${
+			curves.map(([, b, c, d ]) => {
+
+				return ` C ${
+						(b[0] - Ox).toFixed(4)
+					} ${
+						(b[1] - Oy).toFixed(4)
+					}, ${
+						(c[0] - Ox).toFixed(4)
+					} ${
+						(c[1] - Oy).toFixed(4)
+					}, ${
+						(d[0] - Ox).toFixed(4)
+					} ${
+						(d[1] - Oy).toFixed(4)
+					}`;
+			})
+		}"/>\n</svg>`
+	);
+};
 
 onMounted(() => {
 
-	const cnv = canvasRef.value;
+	cnv = canvasRef.value;
 
 	cnv.width = window.innerWidth;
 	cnv.height = window.innerHeight;
 
-	const ctx = canvasRef.value.getContext("2d");
-
-	ctx.strokeStyle = "#ffffff";
-	ctx.fillStyle = "#ffffff";
-	ctx.lineCap = "round";
-	ctx.lineWidth = 1;
-
-	const Ox = 0, Oy = 0;
-
-	const spine = new Spine(100, 10, 14);
-
-	let t0 = 0;
-
-	const render = () => {
-
-		ctx.clearRect(0, 0, cnv.width, cnv.height);
-
-		//const gap = Math.PI / 5 + t0;
-		const gap = 0;
-		let t = -t0;
-		const dt = 0.1 / spine.n;
-	
-		let point = proj( spine.fx(t), spine.fy(t), spine.fz(t) );
-
-		for (t; t < pi2 + dt - gap; t += dt) {
-
-			//ctx.lineWidth = 8 * Math.abs( (2 * t / pi2) - 1 ) + 8;
-
-			ctx.beginPath();
-			ctx.moveTo(Ox + point[0], Oy + point[1]);
-
-			point = proj( spine.fx(t), spine.fy(t), spine.fz(t) );
-			ctx.lineTo(Ox + point[0], Oy + point[1]);
-
-			ctx.stroke();
-		}
-
-		//ctx.beginPath();
-		//ctx.moveTo(...center( proj( spark[0], spark[1], spark[2] ) ) );
-
-		//for (let i = 3; i < spark.length - 8; i += 9) {
-
-		//	ctx.bezierCurveTo(
-		//		...center( proj( spark[i], spark[i + 1], spark[i + 2] ) ),
-		//		...center( proj( spark[i + 3], spark[i + 4], spark[i + 5] ) ),
-		//		...center( proj( spark[i + 6], spark[i + 7], spark[i + 8] ) )
-		//	);
-		//}
-
-		//ctx.lineWidth = 1;
-		//ctx.fill();
-		//ctx.stroke();
-
-		//t0 = (t0 + 0.01) % pi2;
-
-		//requestAnimationFrame(anim);
-	};
+	ctx = canvasRef.value.getContext("2d");
 
 	render();
-
 });
+
+//onUpdated(() => {
+
+//	render();
+//});
+
 </script>
 
 <template>
