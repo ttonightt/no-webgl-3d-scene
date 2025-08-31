a7c2107d2f17454ec6c346e3f530dccb04c96f27
ttonightt <ttonightt@outlook.com>
2025-08-31 15:29:20 +0200

    Perspective works

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { onMounted, ref, useTemplateRef, watch } from "vue";
+const pi2 = Math.PI * 2;
 
-const canvasRef = useTemplateRef("canvas-ref");
 
 const vec = (x, y, z) => ({ x, y, z });
 
@@ -10,6 +10,8 @@ const cross = (v1, v2) => ({
 	y: (v1.z * v2.x) - (v1.x * v2.z),
 	z: (v1.x * v2.y) - (v1.y * v2.x)
 });
+
+const dot = (v1, v2) => (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
 
 const norm = v => {
 
@@ -28,58 +30,82 @@ const diff = (v, v0) => ({
 	z: v.z - v0.z
 });
 
-const mxm = (m1, m2) => {
+const lookAtM = (targetV, cameraV) => {
+
+	const UP = vec(0, 1, 0);
+
+	const L = norm(diff(targetV, cameraV));
+	const s = norm(cross(L, UP));
+	const u = cross(s, L);
 
 	return [
-		(m1[0] * m2[0]) + (m1[1] * m2[3]) + (m1[2] * m2[6]), (m1[0] * m2[0]) + (m1[1] * m2[3]) + (m1[2] * m2[6]), (m1[0] * m2[0]) + (m1[1] * m2[3]) + (m1[2] * m2[6]),
-		(m1[3] * m2[1]) + (m1[4] * m2[4]) + (m1[5] * m2[7]), (m1[3] * m2[1]) + (m1[4] * m2[4]) + (m1[5] * m2[7]), (m1[3] * m2[1]) + (m1[4] * m2[4]) + (m1[5] * m2[7]),
-		(m1[6] * m2[2]) + (m1[7] * m2[5]) + (m1[8] * m2[8]), (m1[6] * m2[2]) + (m1[7] * m2[5]) + (m1[8] * m2[8]), (m1[6] * m2[2]) + (m1[7] * m2[5]) + (m1[8] * m2[8])
+		s.x, s.y, s.z, -dot(s, cameraV),
+		u.x, u.y, u.z, -dot(u, cameraV),
+		-L.x, -L.y, -L.z, dot(L, cameraV),
+		0, 0, 0, 1
 	];
 };
 
-const center = v => {
+const intrinsicM = (f, plainW, plainH) => [
 
-	if (v.length) {
-
-		return [v[0] + O.x, v[1] + O.y];
-	} else {
-		
-		return [v.x + O.x, v.y + O.y];
-	}
-};
-
-const posX = ref(0);
-const posY = ref(0);
-const posZ = ref(0);
-
-const O = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
-const trg = vec(0, 0, 0);
-const cam = vec(0, 100, 100);
-const up = vec(0, 1, 0);
-const asp = window.innerWidth / window.innerHeight;
-const fov = 1;
+	f, 0, plainW / 2,
+	0, f, plainH / 2,
+	0, 0, 1
+];
 
 const proj = (x0, y0, z0) => {
 
-	const L = norm(diff(trg, cam));
-	const s = norm(cross(L, up));
-	const u = cross(s, L);
+	const trg = vec(0, 0, 0);
+	const cam = vec(0, 100, 100);
+	const f = 100;
 
-	const x = (x0 * s.x) + (y0 * s.y) + (z0 * s.z);
-	const y = (x0 * u.x) + (y0 * u.y) + (z0 * u.z);
+	const E = lookAtM(trg, cam);
+	const I = intrinsicM(f, window.innerWidth, window.innerHeight);
 
-	return [x * fov, y * fov];
+	const V = [
+		(E[0] * x0) + (E[1] * y0) +  (E[2] * z0) + E[3],
+		(E[4] * x0) + (E[5] * y0) +  (E[6] * z0) + E[7],
+		(E[8] * x0) + (E[9] * y0) + (E[10] * z0) + E[11]
+	];
+
+	const P = [
+		(I[0] * V[0]) + (I[1] * V[1]) + (I[2] * V[2]),
+		(I[3] * V[0]) + (I[4] * V[1]) + (I[5] * V[2]),
+		(I[6] * V[0]) + (I[7] * V[1]) + (I[8] * V[2]),
+	];
+
+	return [ P[0] / P[2], P[1] / P[2] ];
 };
 
-const R = 100;
-const n = 14;
-const r = 20;
-const pi2 = Math.PI * 2;
-const O3d = vec(0, 0, 0);
+class Spine {
 
-const fx = t => (R + r * Math.cos(n * t)) * Math.cos(t);
-const fy = t => (R + r * Math.cos(n * t)) * Math.sin(t);
-const fz = t => r * Math.sin(n * t);
+	constructor (R, r, n) {
+
+		this.R = R;
+		this.r = r;
+		this.n = n;
+		this.o = [0, 0, 0];
+	}
+
+	moveTo (v) {
+		this.o = [...v];
+	}
+
+	fx (t) {
+		return (this.R + this.r * Math.cos(this.n * t)) * Math.cos(t);
+	}
+
+	fy (t) {
+		return (this.R + this.r * Math.cos(this.n * t)) * Math.sin(t);
+	}
+
+	fz (t) {
+		return this.r * Math.sin(this.n * t);
+	}
+}
+
+
+const canvasRef = useTemplateRef("canvas-ref");
 
 onMounted(() => {
 
@@ -93,52 +119,34 @@ onMounted(() => {
 	ctx.strokeStyle = "#ffffff";
 	ctx.fillStyle = "#ffffff";
 	ctx.lineCap = "round";
+	ctx.lineWidth = 1;
 
-	const peaks = 5;
-	const w0 = 10 * Math.PI / 180;
-	const dw = Math.PI * 2 / peaks;
-	const ppRx = 100;
-	const ppRy = 150;
-	const cpRx = ppRx * 0.05;
-	const cpRy = ppRy * 0.05;
-	const dz = 0;
+	const Ox = 0, Oy = 0;
 
-	let w = w0;
-
-	const spark = [ppRx * Math.cos(w), ppRy * Math.sin(w), dz];
-
-	for (w; w < pi2; w += dw) {
-
-		spark.push(
-			cpRx * Math.cos(w), cpRy * Math.sin(w), dz,
-			cpRx * Math.cos(w + dw), cpRy * Math.sin(w + dw), dz,
-			ppRx * Math.cos(w + dw), ppRy * Math.sin(w + dw), dz
-		);
-	}
+	const spine = new Spine(100, 10, 14);
 
 	let t0 = 0;
 
-	const anim = () => {
+	const render = () => {
+
+		ctx.clearRect(0, 0, cnv.width, cnv.height);
 
 		//const gap = Math.PI / 5 + t0;
 		const gap = 0;
 		let t = -t0;
-		const dt = 0.1 / n;
+		const dt = 0.1 / spine.n;
 	
-		let _xy = proj( O3d.x + fx(t), O3d.y + fy(t), O3d.z + fz(t) );
-
-		ctx.clearRect(0, 0, cnv.width, cnv.height);
+		let point = proj( spine.fx(t), spine.fy(t), spine.fz(t) );
 
 		for (t; t < pi2 + dt - gap; t += dt) {
 
 			//ctx.lineWidth = 8 * Math.abs( (2 * t / pi2) - 1 ) + 8;
-			ctx.lineWidth = 1;
 
 			ctx.beginPath();
-			ctx.moveTo(O.x + _xy[0], O.y + _xy[1]);
+			ctx.moveTo(Ox + point[0], Oy + point[1]);
 
-			_xy = proj( O3d.x + fx(t), O3d.y + fy(t), O3d.z + fz(t) );
-			ctx.lineTo(O.x + _xy[0], O.y + _xy[1]);
+			point = proj( spine.fx(t), spine.fy(t), spine.fz(t) );
+			ctx.lineTo(Ox + point[0], Oy + point[1]);
 
 			ctx.stroke();
 		}
@@ -164,39 +172,18 @@ onMounted(() => {
 		//requestAnimationFrame(anim);
 	};
 
-	//requestAnimationFrame(anim);
-	anim();
+	render();
 
 });
 </script>
 
 <template>
 	<canvas ref="canvas-ref"></canvas>
-	<div>
-		<p>x: {{ posX }}</p>
-		<input :value="posX" @change="e => posX = +e.target.value" type="range" min="-400" max="400" step="1">
-		<p>y: {{ posY }}</p>
-		<input :value="posY" @change="e => posY = +e.target.value" type="range" min="-400" max="400" step="1">
-		<p>z: {{ posZ }}</p>
-		<input :value="posZ" @change="e => posZ = +e.target.value" type="range" min="-400" max="400" step="1">
-	</div>
 </template>
 
 <style scoped>
 canvas {
 	background-color: black;
-	display: block;
-}
-div {
-	position: fixed;
-	top: 0;
-	right: 0;
-	color: aliceblue;
-}
-p {
-	margin: 0;
-}
-input {
 	display: block;
 }
 </style>
